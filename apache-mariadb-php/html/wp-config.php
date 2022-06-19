60ae3bfbc3bc864fa21078f5b625c272a307ae06
Arne Tarara <arne@datafuse.de>
2022-06-20 00:57:41 +0200

    Renamed much; Added apache-php-basic

---
+++ apache-mariadb-php/html/wp-config.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * This has been slightly modified (to read environment variables) for use in Docker.
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress');
+
+/** Database username */
+define( 'DB_USER', 'wordpress');
+
+/** Database password */
+define( 'DB_PASSWORD', 'wordpress' );
+
+
+/** Database hostname */
+define( 'DB_HOST', 'db' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '');
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'c3ce33b29973f63e965d4e749ed6019019cb2feb');
+define( 'SECURE_AUTH_KEY',  '2a5708d2afbd4a6a5491418f3eb6ed009c1aa056');
+define( 'LOGGED_IN_KEY',    '15275ed5945e87511a3f44ae176331a821543df5');
+define( 'NONCE_KEY',        '37e76c84da5f4bd99606a054a92b9f2e8be0fa33');
+define( 'AUTH_SALT',        '88db0e60426d89506fd558fbdadcf050737b7470');
+define( 'SECURE_AUTH_SALT', '1f28fcdfc9a60784ab43d934e9416ce5eb9389e2');
+define( 'LOGGED_IN_SALT',   '54681828a62bc4bbfba7aa004f5aee7fbb65a5c5');
+define( 'NONCE_SALT',       '96bd5c134eebdeacb943ffb38328db7705268120');
+// (See also https://wordpress.stackexchange.com/a/152905/199287)
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+// If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
+// see also https://wordpress.org/support/article/administration-over-ssl/#using-a-reverse-proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false) {
+	$_SERVER['HTTPS'] = 'on';
+}
+// (we include this by default because reverse proxying is extremely common in container environments)
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
--- puppeteer/README.md
+++
@@ -1,7 +0,0 @@
-## Building and starting
-
-`docker compose up`
-
-## Changing after rebuild
-
-If you make changes to the Dockerfile remember to use `docker compose build` to rebuild.
