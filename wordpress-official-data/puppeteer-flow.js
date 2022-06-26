dcdc508fc3a1f0e607a2267a809e5faadc1b3820
Arne Tarara <arne@datafuse.de>
2022-06-26 12:08:57 +0200

    New microtime reporting to console to read notes from STDOUT

--- wordpress-official-data/puppeteer-flow.js
+++ wordpress-official-data/puppeteer-flow.js
@@ -1,10 +1,8 @@
-console.log(
-    "Starting puppeteer.js at time ",
-    process.hrtime()[0] * 1000000000 + process.hrtime()[1] / 1000
-);
 const puppeteer = require("puppeteer");
+const microtime = require("microtime");
 
 (async () => {
+    console.log(microtime.now()," Launching Browser");
     const browser = await puppeteer.launch({
         defaultViewport: {
             width: 1920,
@@ -13,10 +11,6 @@ const puppeteer = require("puppeteer");
         headless: true,
         args: ["--no-sandbox", "--disable-setuid-sandbox"], // Otherwise it won't run on Docker
     });
-    console.log(
-        "Browser launched at ",
-        process.hrtime()[0] * 1000000000 + process.hrtime()[1] / 1000
-    );
 
     const page = await browser.newPage();
     const dimensions = await page.evaluate(() => {
@@ -28,48 +22,22 @@ const puppeteer = require("puppeteer");
     });
     // console.log("Dimensions:", dimensions); // Only to double-check if the dimensions are as expected
 
-    console.log(
-        "Going to home page ",
-        process.hrtime()[0] * 1000000000 + process.hrtime()[1] / 1000
-    );
-
+    console.log(microtime.now()," Home Page");
     await page.goto("http://green-coding-wordpress-apache-data-container:9875", {
         waitUntil: "networkidle2",
     });
 
-
-    console.log(
-        "Connect Success ",
-        process.hrtime()[0] * 1000000000 + process.hrtime()[1] / 1000
-    );
-
-
-    console.log(
-        "Going to contact page ",
-        process.hrtime()[0] * 1000000000 + process.hrtime()[1] / 1000
-    );
-
     await page.screenshot({ path: "/tmp/filled-form.png", fullPage: true });
 
+    console.log(microtime.now()," Contact Page");
     await page.click("a[href='http://green-coding-wordpress-apache-data-container:9875/?page_id=34']");
     await page.waitForSelector(".wpcf7 form input");
 
-    console.log(
-        "Going to long text page ",
-        process.hrtime()[0] * 1000000000 + process.hrtime()[1] / 1000
-    );
-
+    console.log(microtime.now()," Long Text Page");
     await page.click("a[href='http://green-coding-wordpress-apache-data-container:9875/?page_id=36']");
 
     await page.waitForXPath('//*[contains(text(), "Lorem ipsum dolor sit amet")]')
 
+    console.log(microtime.now()," Closing Browser");
     await browser.close();
-    console.log(
-        "Browser closed at ",
-        process.hrtime()[0] * 1000000000 + process.hrtime()[1] / 1000
-    );
 })();
-console.log(
-    "Finishing puppeteer.js at ",
-    process.hrtime()[0] * 1000000000 + process.hrtime()[1] / 1000
-);
--- wordpress-official-starter/puppeteer-flow.js
+++ wordpress-official-starter/puppeteer-flow.js
@@ -1,10 +1,8 @@
-console.log(
-    "Starting puppeteer.js at time ",
-    process.hrtime()[0] * 1000000000 + process.hrtime()[1] / 1000
-);
 const puppeteer = require("puppeteer");
+const microtime = require("microtime");
 
 (async () => {
+    console.log(microtime.now()," Launching browser");
     const browser = await puppeteer.launch({
         defaultViewport: {
             width: 1920,
@@ -13,10 +11,6 @@ const puppeteer = require("puppeteer");
         headless: true,
         args: ["--no-sandbox", "--disable-setuid-sandbox"], // Otherwise it won't run on Docker
     });
-    console.log(
-        "Browser launched at ",
-        process.hrtime()[0] * 1000000000 + process.hrtime()[1] / 1000
-    );
 
     const page = await browser.newPage();
     const dimensions = await page.evaluate(() => {
@@ -28,20 +22,10 @@ const puppeteer = require("puppeteer");
     });
     // console.log("Dimensions:", dimensions); // Only to double-check if the dimensions are as expected
 
-    console.log(
-        "Going to home page ",
-        process.hrtime()[0] * 1000000000 + process.hrtime()[1] / 1000
-    );
-
+    console.log(microtime.now()," Home Page");
     await page.goto("http://green-coding-wordpress-apache-basic-container:9875", {
         waitUntil: "networkidle2",
     });
-
-
-    console.log(
-        "Connect Success ",
-        process.hrtime()[0] * 1000000000 + process.hrtime()[1] / 1000
-    );
 
     // then continue with your code from here:
     //await page.goto("http://green-coding-wordpress-apache-basic-container:9875/my-sample-page", {
@@ -49,13 +33,6 @@ const puppeteer = require("puppeteer");
     //});
 
     // ....
+    console.log(microtime.now()," Closing browser");
     await browser.close();
-    console.log(
-        "Browser closed at ",
-        process.hrtime()[0] * 1000000000 + process.hrtime()[1] / 1000
-    );
 })();
-console.log(
-    "Finishing puppeteer.js at ",
-    process.hrtime()[0] * 1000000000 + process.hrtime()[1] / 1000
-);
