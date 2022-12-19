66c1436c3713808849d6883b757667bd581c1b46
Danilo Ješić <mutant.krofna@gmail.com>
2022-12-19 16:23:46 +0100

    Working test for Chrome running on alpine

---
+++ container-puppeteer-firefox-chrome/puppeteer-image-test-chrome.js
@@ -0,0 +1,37 @@
+const puppeteer = require("puppeteer");
+const microtime = require('microtime');
+
+(async () => {
+    console.log(microtime.now()," Launching Browser");
+    const browser = await puppeteer.launch({
+        defaultViewport: {
+            width: 1920,
+            height: 1080,
+        },
+        headless: false,
+        executablePath: "/usr/bin/chromium-browser",
+        args: ['--no-sandbox']
+    });
+
+    const page = await browser.newPage();
+    const dimensions = await page.evaluate(() => {
+        return {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight,
+            deviceScaleFactor: window.devicePixelRatio,
+        };
+    });
+    // console.log("Dimensions:", dimensions); // Only to double-check if the dimensions are as expected
+
+    console.log(microtime.now()," Home Page");
+    await page.goto("https://www.green-coding.org", { // for this example we are running on port 80
+        waitUntil: "load",
+    });
+
+    await page.waitForSelector(".data-content-one");
+    const text = await page.$eval('.credits', element => element.textContent)
+    console.log(microtime.now(),` Found text content: ${text}`);
+
+    console.log(microtime.now()," Closing Browser");
+    await browser.close();
+})();
--- container-puppeteer-firefox-chrome/puppeteer_chrome.Dockerfile
+++ container-puppeteer-firefox-chrome/puppeteer_chrome.Dockerfile
@@ -1,24 +1,9 @@
-FROM ubuntu:22.04
+FROM node:18-alpine3.16
 
-# PUPPETEER
-RUN apt update \
-    && apt install curl gnupg nano -y \
-    && curl -fsSL https://deb.nodesource.com/setup_16.x | bash - \
-    && curl -fsSL https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
-    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
-    && apt update \
-    && apt install nodejs google-chrome-stable fonts-ipafont-gothic fonts-wqy-zenhei \
-    fonts-freefont-ttf libxss1 --no-install-recommends -y \
-    && rm -rf /var/lib/apt/lists/*
+RUN apk add chromium curl ttf-freefont pciutils udev
 
 WORKDIR /var/www
 COPY ./package.json /var/www/package.json
 RUN npm i
 
-# Chrome somehow cannot start without root and in order to start. This is a quick fix
-RUN chmod -R 777 /var/www/node_modules/puppeteer/.local-chromium/
-RUN chmod 777 /var/www/
-
-# Switching to a non-root user for security concerns
-USER 1001
-CMD ["bash"]
+ENTRYPOINT [ "sh" ]
