88ec7e03c351ee1db82b8bddf9a39743bf745c01
dan-mm <dan@green-coding.berlin>
2023-02-28 17:31:30 +0100

    port checking more robust

--- test/smoke_test.py
+++ test/smoke_test.py
@@ -62,13 +62,71 @@ def run_test_on_directory(directory, capsys, allow_unsafe=False):
     # Assert that there is no std.err output
     assert captured.err == ''
 
+# thanks Didi!
+class Loader(yaml.SafeLoader):
+    def __init__(self, stream):
+        # We need to find our own root as the Loader is instantiated in PyYaml
+        self._root = os.path.split(stream.name)[0]
+        super().__init__(stream)
+
+    def include(self, node):
+        # We allow two types of includes
+        # !include <filename> => ScalarNode
+        # and
+        # !include <filename> <selector> => SequenceNode
+        if isinstance(node, yaml.nodes.ScalarNode):
+            nodes = [self.construct_scalar(node)]
+        elif isinstance(node, yaml.nodes.SequenceNode):
+            nodes = self.construct_sequence(node)
+        else:
+            raise ValueError("We don't support Mapping Nodes to date")
+
+        filename = os.path.realpath(os.path.join(self._root, nodes[0]))
+
+        if not filename.startswith(self._root):
+            raise ImportError(f"Import tries to escape root! ({filename})")
+
+        # To double check we also check if it is in the files allow list
+        if filename not in [os.path.join(self._root, item) for item in os.listdir(self._root)]:
+            print(os.listdir(self._root))
+            raise RuntimeError(f"{filename} not in allowed file list")
+
+
+        with open(filename, 'r', encoding='utf-8') as f:
+            # We want to enable a deep search for keys
+            def recursive_lookup(k, d):
+                if k in d:
+                    return d[k]
+                for v in d.values():
+                    if isinstance(v, dict):
+                        return recursive_lookup(k, v)
+                return None
+
+            # We can use load here as the Loader extends SafeLoader
+            if len(nodes) == 1:
+                # There is no selector specified
+                return yaml.load(f, Loader)
+
+            return recursive_lookup(nodes[1], yaml.load(f, Loader))
+
+Loader.add_constructor('!include', Loader.include)
+
 def check_for_ports(directory):
     if os.path.exists(f"{directory}/usage_scenario.yml"):
         with open(f"{directory}/usage_scenario.yml", encoding='utf8') as f:
-            usage_scenario = yaml.safe_load(f)
-            for service in usage_scenario['services']:
-                if 'ports' in usage_scenario['services'][service]:
-                    return True
+            usage_scenario = yaml.load(f, Loader=Loader)
+            if 'services' in usage_scenario:
+                for service in usage_scenario['services']:
+                    if 'ports' in usage_scenario['services'][service]:
+                        return True
+                        
+    if os.path.exists(f"{directory}/compose.yml"):
+        with open(f"{directory}/compose.yml", encoding='utf8') as f:
+            compose_yml = yaml.load(f, Loader=Loader)
+            if 'services' in compose_yml:
+                for service in compose_yml['services']:
+                    if 'ports' in compose_yml['services'][service]:
+                        return True
     return False
 
 @pytest.mark.parametrize("example_directory", example_directories())
