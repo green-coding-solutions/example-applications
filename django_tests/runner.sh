93e7f635cef4a93fc063eeb940859a41b51dd7b8
Arne Tarara <arne@datafuse.de>
2023-08-03 15:52:22 +0200

    Added Django to own repo. Removed from examples

--- django_tests/runner.sh
+++
@@ -1,4 +0,0 @@
-# We need to wrap the runtests.py so that
-# - all stderr output is redirected to stdout as otherwise the green metric tool (GMT) thinks there is an error
-# - return true at the end as the runtests returns -1 if something failed and we don't want the GMT to fail because a test failed
-python runtests.py --noinput 2>&1 || true
