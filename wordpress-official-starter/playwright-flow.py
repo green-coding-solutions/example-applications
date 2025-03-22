475a1775e009132f5e73e818f0ffa5e51f1d45ec
Arne Tarara <arne@datafuse.de>
2025-03-22 20:05:33 +0100

    Moved Wordpress example to Playwright

---
+++ wordpress-official-starter/playwright-flow.py
@@ -0,0 +1,37 @@
+from time import time_ns
+from playwright.sync_api import sync_playwright
+
+def log_note(message: str) -> None:
+    timestamp = str(time_ns())[:16]
+    print(f"{timestamp} {message}")
+
+with sync_playwright() as playwright:
+    log_note('Launching browser')
+
+    browser = playwright.chromium.launch(headless=True)
+    context = browser.new_context(ignore_https_errors=True, viewport={"width": 1920, "height": 1080})
+    page = context.new_page()
+
+    dimensions = page.evaluate("""
+        () => {
+            return {
+                width: document.documentElement.clientWidth,
+                height: document.documentElement.clientHeight,
+                deviceScaleFactor: window.devicePixelRatio,
+            }
+        }
+    """)
+    # print("Dimensions:", dimensions)  # Optional check
+
+    log_note('Home Page')
+    page.goto("http://gcb-wordpress:9875", wait_until="networkidle")
+
+    # Wait until logo is visible
+    page.locator('p#logo').wait_for(timeout=30)
+
+
+    # Example of navigating to another page:
+    # page.goto("http://gcb-wordpress:9875/my-sample-page", wait_until="networkidle")
+
+    log_note('Closing browser')
+    browser.close()
